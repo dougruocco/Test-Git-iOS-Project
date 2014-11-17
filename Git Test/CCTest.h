e8ad7ff7e5910f06148b55c41f981161435ab4e5
Doug Ruocco <dougruocco@IT-Rooms-iMac.local>
2014-11-17 18:17:29 -0500

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Doug Ruocco on 11/17/14.
+//  Copyright (c) 2014 Doug Ruocco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Doug Ruocco on 11/17/14.
+//  Copyright (c) 2014 Doug Ruocco. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
